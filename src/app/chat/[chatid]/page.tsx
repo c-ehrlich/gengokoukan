d3aebd2c609dce44172789e4bf1915b984258824
c-ehrlich <ehrlich.christopher@gmail.com>
2024-06-22 20:03:16 +0200

    scrolly chat box

--- src/app/chat/[chatid]/page.tsx
+++ src/app/chat/[chatid]/page.tsx
@@ -1,25 +1,40 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { flushSync } from "react-dom";
+import { ScrollArea } from "~/components/_primitives/ui/scroll-area";
 import { api } from "~/trpc/react";
 
 export default function ChatPage() {
+  const scrollToBottomRef = useRef<HTMLDivElement>(null);
   const { chatid } = useParams();
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState<
     { text: string; timestamp: number }[]
   >([]);
 
+  const scrollToBottom = useCallback(() => {
+    scrollToBottomRef.current?.scrollIntoView({ behavior: "instant" });
+  }, []);
+
+  useEffect(() => {
+    scrollToBottom();
+  });
+
   const chatMutation = api.chat.sendMessage.useMutation({
     onSuccess: (data) => {
-      setMessages((prev) => [
-        ...prev,
-        {
-          text: data.message,
-          timestamp: data.timestamp,
-        },
-      ]);
+      flushSync(() => {
+        setMessages((prev) => [
+          ...prev,
+          {
+            text: data.message,
+            timestamp: data.timestamp,
+          },
+        ]);
+      });
+
+      scrollToBottom();
       setInput("");
     },
   });
@@ -27,14 +42,20 @@ export default function ChatPage() {
   return (
     <div>
       <h1>Chat {JSON.stringify(chatid)}</h1>
-      <div className="flex flex-col gap-2">
-        {messages.map((message) => (
-          <div className="bg-blue-200" key={message.timestamp}>
-            <p>{message.text}</p>
-            <p>{new Date(message.timestamp).toLocaleString()}</p>
-          </div>
-        ))}
-      </div>
+      <ScrollArea className="h-72 w-48 rounded-md border">
+        <div className="flex min-h-full flex-col justify-end gap-2">
+          {messages.map((message) => (
+            <div className="bg-blue-200" key={message.timestamp}>
+              <p>{message.text}</p>
+              <p>{new Date(message.timestamp).toLocaleString()}</p>
+            </div>
+          ))}
+          <div
+            style={{ float: "left", clear: "both" }}
+            ref={scrollToBottomRef}
+          ></div>
+        </div>
+      </ScrollArea>
       <div className="flex-row gap-2">
         <input
           className="border p-1"
