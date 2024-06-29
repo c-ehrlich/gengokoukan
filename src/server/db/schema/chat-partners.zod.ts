d8e5401cca3655e315610821caec64eddfbd3c49
c-ehrlich <ehrlich.christopher@gmail.com>
2024-06-29 23:46:41 +0200

    extend create chat partner schema

--- src/server/db/schema/chat-partners.zod.ts
+++ src/server/db/schema/chat-partners.zod.ts
@@ -1,8 +1,12 @@
 import { createInsertSchema } from "drizzle-zod";
 import { chatPartnersTable } from "./chat-partners";
 
-export const createChatPartnerSchemaServer =
-  createInsertSchema(chatPartnersTable);
+export const createChatPartnerSchemaServer = createInsertSchema(
+  chatPartnersTable,
+  {
+    name: (schema) => schema.name.min(1, "名前は1文字以上である必要がある"),
+  },
+);
 
 export const createChatPartnerSchemaClient = createChatPartnerSchemaServer.omit(
   {
